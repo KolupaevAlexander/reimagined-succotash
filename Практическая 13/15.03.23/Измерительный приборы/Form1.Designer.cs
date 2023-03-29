8a43b02a8e4829b62d89466cfa99e75de51e8688
KolupaevAlexander <113934423+KolupaevAlexander@users.noreply.github.com>
2023-03-29 11:49:22 +0500

    Add files via upload

---
+++ Практическая 13/15.03.23/Измерительный приборы/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace Измерительный_приборы
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.удалитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.delete = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.unit = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.minMeasure = new System.Windows.Forms.TextBox();
+            this.maxMeasure = new System.Windows.Forms.TextBox();
+            this.addObject = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.accuracy = new System.Windows.Forms.TextBox();
+            this.producer = new System.Windows.Forms.TextBox();
+            this.type = new System.Windows.Forms.TextBox();
+            this.change = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Size = new System.Drawing.Size(1200, 692);
+            this.splitContainer1.SplitterDistance = 592;
+            this.splitContainer1.SplitterWidth = 6;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(592, 692);
+            this.panel2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(592, 692);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.удалитьToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(124, 36);
+            // 
+            // удалитьToolStripMenuItem
+            // 
+            this.удалитьToolStripMenuItem.Name = "удалитьToolStripMenuItem";
+            this.удалитьToolStripMenuItem.Size = new System.Drawing.Size(123, 32);
+            this.удалитьToolStripMenuItem.Text = "Удалить";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.change);
+            this.panel1.Controls.Add(this.delete);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.unit);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.minMeasure);
+            this.panel1.Controls.Add(this.maxMeasure);
+            this.panel1.Controls.Add(this.addObject);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.accuracy);
+            this.panel1.Controls.Add(this.producer);
+            this.panel1.Controls.Add(this.type);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(602, 692);
+            this.panel1.TabIndex = 0;
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(202, 439);
+            this.delete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(134, 43);
+            this.delete.TabIndex = 13;
+            this.delete.Text = "Удалить";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(55, 190);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(202, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Единица измерения";
+            // 
+            // unit
+            // 
+            this.unit.Location = new System.Drawing.Point(60, 220);
+            this.unit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.unit.Name = "unit";
+            this.unit.Size = new System.Drawing.Size(188, 26);
+            this.unit.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(55, 312);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(314, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Максимальная мера измерения";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(55, 373);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(306, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Минимальная мера измерения";
+            // 
+            // minMeasure
+            // 
+            this.minMeasure.Location = new System.Drawing.Point(60, 403);
+            this.minMeasure.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.minMeasure.Name = "minMeasure";
+            this.minMeasure.Size = new System.Drawing.Size(188, 26);
+            this.minMeasure.TabIndex = 8;
+            // 
+            // maxMeasure
+            // 
+            this.maxMeasure.Location = new System.Drawing.Point(60, 342);
+            this.maxMeasure.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.maxMeasure.Name = "maxMeasure";
+            this.maxMeasure.Size = new System.Drawing.Size(188, 26);
+            this.maxMeasure.TabIndex = 7;
+            // 
+            // addObject
+            // 
+            this.addObject.Location = new System.Drawing.Point(60, 439);
+            this.addObject.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.addObject.Name = "addObject";
+            this.addObject.Size = new System.Drawing.Size(134, 43);
+            this.addObject.TabIndex = 6;
+            this.addObject.Text = "Добавить";
+            this.addObject.UseVisualStyleBackColor = true;
+            this.addObject.Click += new System.EventHandler(this.addObject_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(55, 251);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(210, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Точность измерения";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(55, 129);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(229, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Фирма производитель";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(55, 68);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Тип прибора";
+            // 
+            // accuracy
+            // 
+            this.accuracy.Location = new System.Drawing.Point(60, 281);
+            this.accuracy.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.accuracy.Name = "accuracy";
+            this.accuracy.Size = new System.Drawing.Size(188, 26);
+            this.accuracy.TabIndex = 2;
+            // 
+            // producer
+            // 
+            this.producer.Location = new System.Drawing.Point(60, 159);
+            this.producer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.producer.Name = "producer";
+            this.producer.Size = new System.Drawing.Size(188, 26);
+            this.producer.TabIndex = 1;
+            // 
+            // type
+            // 
+            this.type.Location = new System.Drawing.Point(60, 98);
+            this.type.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.type.Name = "type";
+            this.type.Size = new System.Drawing.Size(188, 26);
+            this.type.TabIndex = 0;
+            // 
+            // change
+            // 
+            this.change.Location = new System.Drawing.Point(60, 492);
+            this.change.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.change.Name = "change";
+            this.change.Size = new System.Drawing.Size(134, 43);
+            this.change.TabIndex = 14;
+            this.change.Text = "Изменить";
+            this.change.UseVisualStyleBackColor = true;
+            this.change.Click += new System.EventHandler(this.change_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Коллекции";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button addObject;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox accuracy;
+        private System.Windows.Forms.TextBox producer;
+        private System.Windows.Forms.TextBox type;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem удалитьToolStripMenuItem;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox minMeasure;
+        private System.Windows.Forms.TextBox maxMeasure;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox unit;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button change;
+    }
+}
+
---
+++ Практическая 13/15.03.23/Измерительный приборы/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Измерительный_приборы
+{
+    public partial class Form1 : Form
+    {
+        private DataGridViewColumn dataGridViewColumn1 = null;
+        private DataGridViewColumn dataGridViewColumn2 = null;
+        private DataGridViewColumn dataGridViewColumn3 = null;
+        private DataGridViewColumn dataGridViewColumn4 = null;
+        private DataGridViewColumn dataGridViewColumn5 = null;
+        private DataGridViewColumn dataGridViewColumn6 = null;
+
+        private SortedList<uint, MeasuringDevice> devices = new SortedList<uint, MeasuringDevice>();
+        public Form1()
+        {
+            InitializeComponent();
+            initDataGridView();
+        }
+
+        //Инициализация таблицы
+        private void initDataGridView()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.Columns.Add(getDataGridViewColumn1());
+            dataGridView1.Columns.Add(getDataGridViewColumn2());
+            dataGridView1.Columns.Add(getDataGridViewColumn3());
+            dataGridView1.Columns.Add(getDataGridViewColumn4());
+            dataGridView1.Columns.Add(getDataGridViewColumn5());
+            dataGridView1.Columns.Add(getDataGridViewColumn6());
+            dataGridView1.AutoResizeColumns();
+        }
+        private DataGridViewColumn getDataGridViewColumn1()
+        {
+            if (dataGridViewColumn1 == null)
+            {
+                dataGridViewColumn1 = new DataGridViewTextBoxColumn();
+                dataGridViewColumn1.Name = "";
+                dataGridViewColumn1.HeaderText = "Тип прибора";
+                dataGridViewColumn1.ValueType = typeof(string);
+                dataGridViewColumn1.Width = dataGridView1.Width / 6;
+            }
+            return dataGridViewColumn1;
+        }
+        private DataGridViewColumn getDataGridViewColumn2()
+        {
+            if (dataGridViewColumn2 == null)
+            {                     
+                dataGridViewColumn2 = new DataGridViewTextBoxColumn();
+                dataGridViewColumn2.Name = "";
+                dataGridViewColumn2.HeaderText = "Фирма";
+                dataGridViewColumn2.ValueType = typeof(string);
+                dataGridViewColumn2.Width = dataGridView1.Width / 6;
+            }
+            return dataGridViewColumn2;
+        }
+        private DataGridViewColumn getDataGridViewColumn3()
+        {
+            if (dataGridViewColumn3 == null)
+            {                     
+                dataGridViewColumn3 = new DataGridViewTextBoxColumn();
+                dataGridViewColumn3.Name = "";
+                dataGridViewColumn3.HeaderText = "Единица измерения";
+                dataGridViewColumn3.ValueType = typeof(string);
+                dataGridViewColumn3.Width = dataGridView1.Width / 6;
+            }
+            return dataGridViewColumn3;
+        }
+        private DataGridViewColumn getDataGridViewColumn4()
+        {
+            if (dataGridViewColumn4 == null)
+            {                     
+                dataGridViewColumn4 = new DataGridViewTextBoxColumn();
+                dataGridViewColumn4.Name = "";
+                dataGridViewColumn4.HeaderText = "Точность";
+                dataGridViewColumn4.ValueType = typeof(string);
+                dataGridViewColumn4.Width = dataGridView1.Width / 6;
+            }
+            return dataGridViewColumn4;
+        }
+        private DataGridViewColumn getDataGridViewColumn5()
+        {
+            if (dataGridViewColumn5 == null)
+            {                     
+                dataGridViewColumn5 = new DataGridViewTextBoxColumn();
+                dataGridViewColumn5.Name = "";
+                dataGridViewColumn5.HeaderText = "Максимальное значение";
+                dataGridViewColumn5.ValueType = typeof(string);
+                dataGridViewColumn5.Width = dataGridView1.Width / 6;
+            }
+            return dataGridViewColumn5;
+        }
+        private DataGridViewColumn getDataGridViewColumn6()
+        {
+            if (dataGridViewColumn6 == null)
+            {                     
+                dataGridViewColumn6 = new DataGridViewTextBoxColumn();
+                dataGridViewColumn6.Name = "";
+                dataGridViewColumn6.HeaderText = "Минимальное значение";
+                dataGridViewColumn6.ValueType = typeof(string);
+                dataGridViewColumn6.Width = dataGridView1.Width / 6;
+            }
+            return dataGridViewColumn6;
+        }
+
+        //Обработчики событий
+        private void addObject_Click(object sender, EventArgs e)
+        {
+            AddDevice(type.Text, producer.Text, accuracy.Text, maxMeasure.Text, minMeasure.Text, unit.Text);    
+        }
+        private void delete_Click(object sender, EventArgs e)
+        {
+            if (devices.Count > 0)
+            {
+                int selectedRow = dataGridView1.SelectedCells[0].RowIndex;
+                DialogResult dr = MessageBox.Show("Удалить прибор?", "", MessageBoxButtons.YesNo);
+                if (dr == DialogResult.Yes)
+                {
+                    try
+                    {
+                        DeleteDevice(selectedRow);
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+            }
+        }
+        private void change_Click(object sender, EventArgs e)
+        {
+            int sel = dataGridView1.SelectedCells[0].RowIndex;
+            if (change.Text=="Изменить" && sel!=-1)
+            {
+                change.Text = "Подтвердить";
+                type.Text = (string)dataGridView1[0,sel].Value;
+                producer.Text = (string)dataGridView1[1, sel].Value;
+                unit.Text = (string)dataGridView1[2, sel].Value;
+                accuracy.Text = (string)dataGridView1[3, sel].Value;
+                maxMeasure.Text = (string)dataGridView1[4, sel].Value;
+                minMeasure.Text = (string)dataGridView1[5, sel].Value;
+            }
+            else if (type.Text != "" && producer.Text != "" && unit.Text != "" && accuracy.Text != "" && maxMeasure.Text != "" && minMeasure.Text != "")
+            {
+                change.Text = "Изменить";
+                dataGridView1[0, sel].Value = type.Text;
+                dataGridView1[1, sel].Value = producer.Text;
+                dataGridView1[2, sel].Value = unit.Text;
+                dataGridView1[3, sel].Value = accuracy.Text;
+                dataGridView1[4, sel].Value = maxMeasure.Text;
+                dataGridView1[5, sel].Value = minMeasure.Text;
+            }
+            else MessageBox.Show("Пустые поля недопустимы");
+        }
+
+        //Заполнение таблицы
+        private void ShowListInGrid()
+        {
+            dataGridView1.Rows.Clear();
+            foreach (MeasuringDevice device in devices.Values)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                DataGridViewTextBoxCell cell1 = new
+                DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell cell2 = new
+                DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell cell3 = new
+                DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell cell4 = new
+                DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell cell5 = new
+                DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell cell6 = new
+                DataGridViewTextBoxCell();
+                cell1.ValueType = typeof(string);
+                cell1.Value = device.Type;
+                cell2.ValueType = typeof(string);
+                cell2.Value = device.Producer;
+                cell3.ValueType = typeof(string);
+                cell3.Value = device.Unit;
+                cell4.ValueType = typeof(string);
+                cell4.Value = device.Accuracy;
+                cell5.ValueType = typeof(string);
+                cell5.Value = device.MaxOfMeasure;
+                cell6.ValueType = typeof(string);
+                cell6.Value = device.MinOfMeasure;
+                row.Cells.Add(cell1);
+                row.Cells.Add(cell2);
+                row.Cells.Add(cell3);
+                row.Cells.Add(cell4);
+                row.Cells.Add(cell5);
+                row.Cells.Add(cell6);
+                dataGridView1.Rows.Add(row);
+            }
+        }
+
+        //Функионал
+        uint counter = 0;
+        private void AddDevice(string type, string producer, string accuracy, string maxOfMeasure, string minOfMeasure, string unit)
+        {
+            counter++;
+            if (this.type.Text != "" && this.producer.Text != "" && this.unit.Text != "" && this.accuracy.Text != "" && this.maxMeasure.Text != "" && this.minMeasure.Text != "")
+            {
+                MeasuringDevice s = new MeasuringDevice(type, producer, accuracy, maxOfMeasure, minOfMeasure, unit);
+                devices.Add(counter, s);
+                this.type.Text = "";
+                this.producer.Text = "";
+                this.unit.Text = "";
+                this.accuracy.Text = "";
+                this.maxMeasure.Text = "";
+                this.minMeasure.Text = "";
+                ShowListInGrid();
+            }
+            else
+            {
+                MessageBox.Show("Пустые поля недопустимы");
+            }
+        }
+        private void DeleteDevice(int elementIndex)
+        {
+            devices.RemoveAt(elementIndex);
+            ShowListInGrid();
+        }
+
+    }
+}
+
